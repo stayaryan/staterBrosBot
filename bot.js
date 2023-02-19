8dc992390f1f2cb73b5e0501ce9cd625f8987c49
stayaryan <aryanam2000@gmail.com>
2023-02-19 02:17:59 -0800

    UpdatedTargetLink

--- bot.js
+++ bot.js
@@ -2,13 +2,14 @@ const axios = require('axios');
 const MPlayer = require('mplayer');
 const player = require('play-sound')();
 
-var url = "https://grocerysupportpickup.youcanbook.me/";
+var urlS = "https://grocerysupportpickup.youcanbook.me/";
 //var url2 = "https://web.whatsapp.com/";
 //var url3 = "https://www.youtube.com/";
+var urlT = "https://grocerysupportecards.youcanbook.me/";
 var url2 = "https://rpantry.youcanbook.me/";
 
 function monitorChange(){
-axios.get(url).then((res)=>{
+axios.get(urlT).then((res)=>{
     if(res.data.includes('This means it')){
         console.log('Unavailable');
     }else{
